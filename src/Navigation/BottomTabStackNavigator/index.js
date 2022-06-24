ba2ba795e0b0879a3c8fa45f28d03a5e30f0d07b
Giosevid <giosevid@gmail.com>
2022-06-24 19:44:54 -0300

    Bottom tab configured

---
+++ src/Navigation/BottomTabStackNavigator/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>SettingsScreen Screen</Text>
+    </View>
+  );
+}
+
+export default function BottomTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  );
+}
