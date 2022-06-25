e037f5b15cc7a2776494cb14f92ede645e99bf30
Giosevid <giosevid@gmail.com>
2022-06-24 21:04:14 -0300

    React native paper added

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {Provider as PaperProvider} from 'react-native-paper';
 import BottomTabs from './src/Navigation/BottomTabStackNavigator';
 import {noHeader} from './src/Navigation/options';
 
@@ -8,10 +9,12 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={noHeader} initialRouteName="Main">
-        <Stack.Screen name="Main" component={BottomTabs} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={noHeader} initialRouteName="Main">
+          <Stack.Screen name="Main" component={BottomTabs} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
