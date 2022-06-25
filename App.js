d8cb08e12e346cc6427aa61c20c08e747ab00955
Giosevid <giosevid@gmail.com>
2022-06-24 22:29:16 -0300

    Update podfile

--- App.js
+++ App.js
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Provider as PaperProvider} from 'react-native-paper';
-import {noHeader} from './src/Navigation/options';
-import {stackTabsRoutes} from './src/Navigation/Routes';
 import StackNavigator from './src/Navigation/StackNavigator';
-
-const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
--- src/Navigation/StackNavigator/index.js
+++ src/Navigation/StackNavigator/index.js
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {stackTabsRoutes} from '../Routes';
+import Routes, {stackTabsRoutes} from '../Routes';
 import {noHeader} from '../options';
 
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
   return (
-    <Stack.Navigator screenOptions={noHeader} initialRouteName="Main">
+    <Stack.Navigator screenOptions={noHeader} initialRouteName={Routes.MAIN}>
       {stackTabsRoutes.map(component => (
         <Stack.Screen
           key={component.name}
